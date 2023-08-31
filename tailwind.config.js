91fd3a2f528d1f4b7cd2d131e475767a7c92fdd5
heiybb <hf.heiybb@gmail.com>
2023-08-31 11:27:24 +1000

    Use github markdown code style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@ module.exports = {
   content: ['./layouts/**/*.html'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            code: {
+              backgroundColor: theme('#25292e'),
+              borderRadius: theme('borderRadius.sm'),
+              paddingTop: theme('padding[1]'),
+              paddingRight: theme('padding[1.5]'),
+              paddingBottom: theme('padding[1]'),
+              paddingLeft: theme('padding[1.5]'),
+            },
+            'code::before': {
+              content: 'normal',
+            },
+            'code::after': {
+              content: 'normal',
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
